6f48d4e5207f5294020fef24537a70866d1b9487
HÁ <haidee.alvarez.c@gmail.com>
2024-09-11 11:56:29 +0100

    added new cities

--- src/index.js
+++ src/index.js
@@ -1,41 +1,51 @@
 function updateTime() {
   let losAngelesElement = document.querySelector("#losangeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let lisbonElement = document.querySelector("#lisbon");
-  let lisbonDateElement = lisbonElement.querySelector(".date");
-  let lisbonTimeElement = lisbonElement.querySelector(".time");
-  let lisbonTime = moment().tz("Europe/Lisbon");
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
+    let lisbonTimeElement = lisbonElement.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
 
-  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
-  lisbonTimeElement.innerHTML = lisbonTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+    lisbonTimeElement.innerHTML = lisbonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = tokyoElement.querySelector(".date");
-  let tokyoTimeElement = tokyoElement.querySelector(".time");
-  let tokyoTime = moment().tz("Asia/Tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
 
-  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let luandaElement = document.querySelector("#luanda");
-  let luandaDateElement = luandaElement.querySelector(".date");
-  let luandaTimeElement = luandaElement.querySelector(".time");
-  let luandaTime = moment().tz("Africa/Luanda");
+  if (luandaElement) {
+    let luandaDateElement = luandaElement.querySelector(".date");
+    let luandaTimeElement = luandaElement.querySelector(".time");
+    let luandaTime = moment().tz("Africa/Luanda");
 
-  luandaDateElement.innerHTML = luandaTime.format("MMMM Do YYYY");
-  luandaTimeElement.innerHTML = luandaTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    luandaDateElement.innerHTML = luandaTime.format("MMMM Do YYYY");
+    luandaTimeElement.innerHTML = luandaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
